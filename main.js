a0d55898476606a5e075dbe597f055b1a42e4de4
yagawa <yagawa@qzc.co.jp>
2019-07-23 11:50:47 +0900

    Add wallpaper

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 const screenshot = require("screenshot-desktop");
+const wallpaper = require("wallpaper");
 const fs = require("fs");
 
-screenshot()
-  .then(function(img){
-    fs.writeFileSync("./snap.jpg", img);
+
+function takeScreenShot(){
+  return screenshot()
+    .then(function(img){
+      const filename = "./snap.jpg"
+      fs.writeFileSync(filename, img);
+      return filename;
+    })
+    .catch(function(err){
+
+    });
+}
+
+
+function setWallPaper(imgname){
+  wallpaper.set(imgname)
+    .then(function(res){
+
+    })
+    .catch(function(err){
+
+    });
+
+}
+
+
+takeScreenShot()
+  .then(function(iname){
+    setWallPaper(iname);
   })
   .catch(function(err){
 
