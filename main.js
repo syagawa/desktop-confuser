9e54a6b654e9e9bd4b5f6369ebd94ab8508afee7
yagawa <yagawa@qzc.co.jp>
2019-07-24 12:44:13 +0900

    Add log4js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const screenshot = require("screenshot-desktop");
 const wallpaper = require("wallpaper");
 const fs = require("fs");
+const log4js = require("log4js");
+
+const appname = "desktop-confuser";
 
 
 function takeScreenShot(){
   return screenshot()
     .then(function(img){
-      const filename = "./snap.jpg"
+      const filename = "./snap.jpg";
       fs.writeFileSync(filename, img);
       return filename;
     })
@@ -27,14 +30,20 @@ function setWallPaper(imgname){
 
 }
 
-setInterval(function() {
-  console.log("shot");
-  takeScreenShot()
-    .then(function(iname){
-      setWallPaper(iname);
-    })
-    .catch(function(err){
+function run(){
+  const logger = log4js.getLogger(appname);
+  logger.level = "debug";
+  logger.info("START!!");
 
-    });
-}, 1000);
+  setInterval(function() {
+    takeScreenShot()
+      .then(function(iname){
+        setWallPaper(iname);
+      })
+      .catch(function(err){
 
+      });
+  }, 1000);
+}
+
+module.exports = run;
